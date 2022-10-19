1399ba7ff67e4b9c58f19b9e374a6c93a7a2bcad
FelipeRojasF <sebas3164@gmail.com>
2022-10-19 16:30:36 -0500

    fixing migrations

--- messaging-api/src/config/migrations/01-initial-db-structure.sql
+++ messaging-api/src/config/migrations/01-initial-db-structure.sql
@@ -6,10 +6,8 @@ CREATE TABLE messages (
    id serial primary key,
    body  varchar(160) not null,
    dateSent varchar(50) not null,
-   receiver varchar(20) not null,
-   CONSTRAINT messages_pk PRIMARY KEY (id)
+   receiver varchar(20) not null
 );
-CREATE INDEX messages_dateSent_time_date_idx ON event ((dateSent::VARCHAR));
 
 INSERT INTO messages (body,dateSent,receiver) values ('Bienvenido a la API', NOW(), '+1222222222');
 INSERT INTO messages (body,dateSent,receiver) values ('este mensaje fue enviado gracias a la API de Twilio', NOW(), '+1222222222');
